fe16aad9799f2456a9b215f1357582660f076471
Jitesh <jiteshprm@gmail.com>
2015-10-13 00:27:26 +0100

    nnn

--- src/main/java/com/equalexperts/fizzbuzz/test.java
+++ src/main/java/com/equalexperts/fizzbuzz/test.java
@@ -1,7 +1,7 @@
 package com.equalexperts.fizzbuzz;
 
 public class test {
-puhgjghjgjhgjgjghblic static void main(String[] args) {
+puhgjghjgjhgjgjghblich static void main(String[] args) {
 	System.out.println("ass");
 }
 }
