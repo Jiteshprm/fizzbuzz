d1621927e845a409f8ecdb25a2d1d0ea9e36e353
Jitesh <jiteshprm@gmail.com>
2015-10-13 00:36:56 +0100

    bbbbb

--- src/main/java/com/equalexperts/fizzbuzz/test.java
+++ src/main/java/com/equalexperts/fizzbuzz/test.java
@@ -1,12 +1,7 @@
 package com.equalexperts.fizzbuzz;
 
 public class test {
-<<<<<<< HEAD
-puhgjghjgjhgjgjghblich static void main(String[] args) {
-	System.out.println("ass");
-=======
 puhgjghjgjhgjgjghblic static void main(String[] args) {
-	
->>>>>>> branch 'master' of https://github.com/Jiteshprm/fizzbuzz.git
+	System.out.println("ass");
 }
 }
